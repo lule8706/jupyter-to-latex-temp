64f2c83e75f0d1b71014e2f970513d7faa6288bc
lleibfried <lleibfried@vescent.com>
2021-11-08 11:50:14 -0700

    Initial commit

---
+++ converter.py
@@ -0,0 +1,24 @@
+from traitlets.config import Config
+import nbformat as nbf
+from nbconvert.exporters import LatexExporter
+from nbconvert.preprocessors import TagRemovePreprocessor
+
+c = Config()
+
+c.TagRemovePreprocessor.remove_cell_tags = ("remove_cell",)
+c.TagRemovePreprocessor.remove_all_outputs_tags = ("remove_output",)
+c.TagRemovePreprocessor.remove_input_tags = ("remove_input",)
+c.TagRemovePreprocessor.enabled = True
+
+c.LatexExporter.preprocessors = ["nbconvert.preprocessors.TagRemovePreprocessor", "nbconvert.preprocessors.ConvertFiguresPreprocessor"]
+c.LatexExporter.template_file = "latex_tmpl/thmsInNb_article.tplx"
+
+exporter = LatexExporter(config=c)
+exporter.register_preprocessor(TagRemovePreprocessor(config=c), True)
+
+output = LatexExporter(config=c).from_filename("final.ipynb")
+
+print(output)
+
+with open("final.tex", "w") as f:
+    f.write(output[0])
